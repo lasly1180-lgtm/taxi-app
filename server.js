cf2fbb49721608e84c9a4f87f9977a39dff5dc1d
sylvie dewerchin <lasly1180@gmail.com>
2026-04-20 20:53:01 +0200

    fix async login

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 /* LOGIN */
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
 const user = db.prepare(
